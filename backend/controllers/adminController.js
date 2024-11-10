bad6f668f2309861ea8bc8843330f67bac588ada
Abdulla Rahman <abdulla30r@gmail.com>
2024-11-10 14:47:01 +0600

    addDoctor complete

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,17 +1,59 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+
 // API for adding doctor
 const addDoctor = async (req, res) => {
   try {
     const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
-    const imageFile = req.file.path;
-    
+    const imageFile = req.file;
+
+    // check if all field are filled
     if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
       console.log("all fields are required");
       return res.json({ success: false, message: "All fields are required" });
-    } else {
-      res.json("ok");
     }
+
+    // check if email is valid
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Invalid email" });
+    }
+
+    // check if password is at least 8 characters long
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Password must be at least 8 characters long" });
+    }
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // upload image to cloudinary
+    const imgUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+    const imageUrl = imgUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      image: imageUrl,
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+    res.json({ success: true, message: "Doctor added successfully" });
   } catch (error) {
-    res.json(error);
+    console.log(error);
+
+    res.json({ success: false, message: error.message });
   }
 };
 
