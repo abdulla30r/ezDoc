bf036a75b7f54baa4bd5febcdf298a5fb9d5d1b6
Abdulla Rahman <abdulla30r@gmail.com>
2024-11-10 20:02:46 +0600

    admin login complete

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,12 +3,16 @@ import { AdminContext } from "./context/AdminContext";
 import { useContext } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <ToastContainer />
+    <div className="bg-[#f8f9fd]">
+      <ToastContainer />
+      <Navbar />
+    </div>
   ) : (
     <div>
       <Login />
