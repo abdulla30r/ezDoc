326b250c30f7980faf55096d4cc52bab6cc48a6b
Abdulla Rahman <abdulla30r@gmail.com>
2024-11-15 03:08:17 +0600

    profile page ta sundor korte hobe

--- admin/src/pages/Doctor/DocDash.jsx
+++ admin/src/pages/Doctor/DocDash.jsx
@@ -53,10 +53,12 @@ const DocDash = () => {
           <div className="pt-4 border border-t-0">
             {docDashData.latestAppointments?.map((item, index) => (
               <div className="flex items-center px-6 py-3 gap-3 hover: bg-gray-100 " key={index}>
-                <img className="w-10 rounded-full" src={item.docData.image} alt="" />
+                <img className="w-10 rounded-full" src={item.userData.image} alt="" />
                 <div className="flex-1 text-sm">
-                  <p className="text-gray-800 font-medium">{item.docData.name}</p>
-                  <p className="text-gray-800 font-medium">{item.slotDate}</p>
+                  <p className="text-gray-800 font-medium">{item.userData.name}</p>
+                  <p className="text-gray-800 font-medium">
+                    {item.slotDate}, {item.slotTime}
+                  </p>
                 </div>
                 {item.payment && !item.isCompleted && <p className="text-yellow-500">Pending</p>}
                 {!item.payment && !item.cancelled && <p className="text-yellow-500">Waiting for payment</p>}
--- admin/src/pages/Doctor/DocProfile.jsx
+++ admin/src/pages/Doctor/DocProfile.jsx
@@ -1,14 +1,51 @@
 import { useEffect } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets/assets";
+import { toast } from "react-toastify";
+import axios from "axios";
 const DocProfile = () => {
-  const { dToken, getDoctorData, docData } = useContext(DoctorContext);
+  const { dToken, backendurl, setDocData, getDoctorData, docData } = useContext(DoctorContext);
+  const { currency } = useContext(AppContext);
 
   useEffect(() => {
     getDoctorData();
   }, [dToken]);
 
-  return <div>{console.log(docData)}</div>;
+  return (
+    <div className="ml-10">
+      <div>
+        <img className="w-72" src={docData.image} alt="" />
+      </div>
+      <div className="mt-4 text-center">
+        <h2 className="text-xl font-semibold text-gray-800">{docData.name}</h2>
+        <p className="text-gray-500">{docData.speciality}</p>
+        <p className="text-gray-400 text-sm">{docData.degree}</p>
+
+        <p className="text-gray-400 text-sm">{docData.experience} of experience</p>
+        <p className="mt-2 text-gray-700">{docData.about}</p>
+      </div>
+
+      <div className="mt-4">
+        <h3 className="text-lg font-medium text-gray-800">Address</h3>
+        <p className="text-gray-600">{docData.address?.line1}</p>
+        <p className="text-gray-600">{docData.address?.line2}</p>
+      </div>
+
+      <div className="mt-4 flex justify-between items-center">
+        <p className="text-lg font-medium text-gray-800">
+          Fees:{" "}
+          <span className="font-semibold">
+            {currency} {docData.fees}
+          </span>
+        </p>
+        <p className="text-lg font-medium text-gray-800">
+          Available: {docData.available && "yes"} {!docData.available && "no"}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default DocProfile;
