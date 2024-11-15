0d111b86d03276c231e72e587d895fbabc9a9369
Abdulla Rahman <abdulla30r@gmail.com>
2024-11-15 16:12:07 +0600

    port changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  server: {
+    port: 8080, // Set the port to 8080
+    host: "0.0.0.0", // Ensure the server is accessible externally
+  },
+});
